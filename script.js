7a47d2ca0f40ca6275186f1fcf1c2c237ceb9d8b
gabe-santos <34319104+gabe-santos@users.noreply.github.com>
2023-03-22 08:38:07 -0700

    creates function compatibility with new itinerary button

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ const itineraryList = document.querySelector('#itinerary-list');
 let itineraryListData = [];
 
 const sampleData = {
-	title: 'Lunch Date w/ Benildo',
-	date: new Date().toLocaleDateString(),
-	activityCount: 2,
-	description: 'getting korean bbq, playing overwatch 2',
-	details: '',
+  title: 'Lunch Date w/ Benildo',
+  date: new Date().toLocaleDateString(),
+  activityCount: 2,
+  description: 'getting korean bbq, playing overwatch 2',
+  details: '',
 };
 
 itineraryListData.push(sampleData);
 
 function loadEventDisplay() {
-	document.querySelector('.event-title').textContent = sampleData.title;
-	document.querySelector('.event-date').textContent =
-		'Date: ' + sampleData.date;
-	document.querySelector('.event-activity-count').textContent =
-		'Activities: ' + sampleData.activityCount;
+  document.querySelector('.event-title').textContent = sampleData.title;
+  document.querySelector('.event-date').textContent =
+    'Date: ' + sampleData.date;
+  document.querySelector('.event-activity-count').textContent =
+    'Activities: ' + sampleData.activityCount;
 }
 
 function loadItineraryList() {
-	// itineraryListData.forEach(item => {
-	// 	itineraryList.appendChild('<li>' + item.title + '</li>');
-	// });
+  // itineraryListData.forEach(item => {
+  // 	itineraryList.appendChild('<li>' + item.title + '</li>');
+  // });
 
-	console.log(itineraryList);
+  console.log(itineraryList);
 
-	let newItinerary = document.createElement('li');
-	newItinerary.textContent = sampleData.title;
+  let newItinerary = document.createElement('li');
+  newItinerary.textContent = sampleData.title;
 
-	itineraryList.append(newItinerary);
+  itineraryList.append(newItinerary);
+}
+
+function createNewItinerary() {
+  console.log('working...');
 }
 
 loadItineraryList();
