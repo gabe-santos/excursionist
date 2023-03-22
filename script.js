c0e8fc397cbc315239138ae653344be89151f8b2
Gabe <28gabe@gmail.com>
2023-03-21 20:34:29 -0700

    basic layout done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const itineraryList = document.querySelector('.itinerary-list');
+const itineraryList = document.querySelector('#itinerary-list');
 let itineraryListData = [];
 
 const sampleData = {
 	title: 'Lunch Date w/ Benildo',
-	date: new Date(),
+	date: new Date().toLocaleDateString(),
 	activityCount: 2,
 	description: 'getting korean bbq, playing overwatch 2',
 	details: '',
@@ -13,12 +13,23 @@ itineraryListData.push(sampleData);
 
 function loadEventDisplay() {
 	document.querySelector('.event-title').textContent = sampleData.title;
+	document.querySelector('.event-date').textContent =
+		'Date: ' + sampleData.date;
+	document.querySelector('.event-activity-count').textContent =
+		'Activities: ' + sampleData.activityCount;
 }
 
 function loadItineraryList() {
-	itineraryListData.forEach(item => {
-		itineraryList.appendChild('<li>' + item.title + '</li>');
-	});
+	// itineraryListData.forEach(item => {
+	// 	itineraryList.appendChild('<li>' + item.title + '</li>');
+	// });
+
+	console.log(itineraryList);
+
+	let newItinerary = document.createElement('li');
+	newItinerary.textContent = sampleData.title;
+
+	itineraryList.append(newItinerary);
 }
 
 loadItineraryList();
