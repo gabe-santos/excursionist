f1d64b3178b07e298e47b5758e2259ada0be7f6e
Gabe <28gabe@gmail.com>
2023-03-29 23:02:56 -0700

    adds close button hover styling

--- src/formDataSelectors.js
+++ src/formDataSelectors.js
@@ -1,6 +1,6 @@
 export const formModal = document.querySelector('.modal');
 export const formModalSubmit = document.querySelector('#form-submit');
-export const formModalClose = document.querySelector('.close');
+export const formModalClose = document.querySelector('.close-btn');
 export const formEventTitle = document.querySelector('#form-title');
 export const formEventDate = document.querySelector('#form-date');
 export const formActivityCount = document.querySelector('#form-activity-count');
