0b9b2a7f3d2f32aef16a47fc0df77482e1332a31
Gabe <28gabe@gmail.com>
2023-04-15 18:44:00 -0700

    fixes activity split

--- src/itineraryList.js
+++ src/itineraryList.js
@@ -1,24 +1,25 @@
 import {
-	formEventTitle,
-	formEventDateStart,
-	formEventDateEnd,
-	formEventDescription,
-} from './selectors';
-import { Itinerary } from './Itinerary';
-import { updateUI } from './uiUpdates';
-import { closeFormModal } from './formModal';
+  formEventTitle,
+  formEventDateStart,
+  formEventDateEnd,
+  formEventDescription,
+} from "./selectors";
+import { Itinerary } from "./Itinerary";
+import { updateUI } from "./uiUpdates";
+import { closeFormModal } from "./formModal";
 
 export const itineraryList = {};
 
 export const addNewItinerary = (title, dateStart, dateEnd, desc) => {
-	const activities = formEventDescription.value.split(/[\n-]+/);
+  console.log(desc);
+  const activities = desc.split(/[\n-]+/); // Split the description into an array of activities
 
-	const newEntry = new Itinerary(title, dateStart, dateEnd, activities);
+  const newEntry = new Itinerary(title, dateStart, dateEnd, activities);
 
-	itineraryList[newEntry.key] = newEntry;
+  itineraryList[newEntry.key] = newEntry;
 
-	// Update the UI to display the newly created Itinerary
-	updateUI(itineraryList);
+  // Update the UI to display the newly created Itinerary
+  updateUI(itineraryList);
 
-	closeFormModal();
+  closeFormModal();
 };
