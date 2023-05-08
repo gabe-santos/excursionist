1eafcfe1428606dafd886cf768f93f38e0b47d6a
Gabe <28gabe@gmail.com>
2023-05-08 13:48:32 -0700

    adds type security

--- src/Itinerary.js
+++ src/Itinerary.js
@@ -7,6 +7,15 @@ export class Itinerary {
 		this.dateStart = new Date(dateStart);
 		this.dateEnd = new Date(dateEnd);
 		this.activities = activities;
+
+		if (
+			Array.isArray(activities) &&
+			activities.every(item => typeof item === 'string')
+		) {
+			this.activities = activities;
+		} else {
+			throw new Error('Activities must be an array of strings.');
+		}
 	}
 	getDateString() {
 		return this.date.toLocaleDateString();
